4e22a50298dc5087161a7b7f2080f98f9fd3e7c0
Jordan Niedzielski <31696469+syrekable@users.noreply.github.com>
2022-02-03 22:44:42 +0000

    my life

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ t0 = abs(int(input('Podaj punkt w czasie w którym chcesz sprawdzić stan wody w
 
 interval = np.linspace(0, t0, num = 100 if t0 > 0 else 1, endpoint=True)
 
+print('-'*20)
+for t in interval:
+  print(f'Czas:{t:>20}\tWysokość wody:{w.water_height(t):>20}\tStan regulatora:{w.regulator(t):>7}')
+
 plt.plot(interval, [w.water_height(t) for t in interval])
 plt.suptitle('Poziom wody w zbiorniku / czas')
 plt.xlabel('czas [s]')
 plt.ylabel('poziom wody w zbiorniku [m]')
 
 plt.show()
+
+print('Do zobaczenia!')
--- water_tank.py
+++ water_tank.py
@@ -12,18 +12,28 @@ class WaterTank:
   qi: float
   qo: float
   max_height: float
-  state: str
 
-  def __init__(self, a, qi, qo, max_height):
+  def __init__(self, a, qi, qo, max_height, regulator: str = 'CLOSED'):
     self.a = a
     self.qi = qi
     self.qo = qo
     self.max_height = max_height
     self.v = self.qi-self.qo # according to lab notes, this holds true whenever 'qi' and 'qo' are constants
 
-  def water_height(self, t) -> float:
+  def water_height(self, t: float) -> float:
     h = self._water_height(t)
+
     return h if h <= self.max_height else self.max_height 
 
   def _water_height(self, t: int) -> float:
     return (self.v*t) / self.a
+
+  def regulator(self, t: float) -> str:
+    h = self._water_height(t)
+
+    if h == 0:
+      return 'OPENED'
+    elif h <= self.max_height:
+      return 'OPEN'
+    else:
+      return 'CLOSED'
